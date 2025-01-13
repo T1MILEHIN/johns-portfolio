4c05fa6a49f65ff1191c7381cd906d6d4eac492a
Timi <tunmisebabade@gmail.com>
2025-01-13 23:20:30 +0100

    first commit

--- src/pages/allProjects.jsx
+++ src/pages/allProjects.jsx
@@ -28,24 +28,30 @@ const AllProjects = () => {
   return (
     <>
       <div className="pt-32 px-10">
-        <h1 className="font-bold text-[64px] leading-[83.2px] font-specify_exp_med py-10">Recent Projects</h1>
-        <div className="flex items-center gap-[18.61px]">
-          <Button onClick={(e) => handleCategories(e, "All")} className={`${CATEGORY === "All" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>All</Button>
-          <Button onClick={(e) => handleCategories(e, "app")} className={`${CATEGORY === "app" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>Applications</Button>
-          <Button onClick={(e) => handleCategories(e, "Website")} className={`${CATEGORY === "Website" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>Website</Button>
-          <Button className="blue_footer_hover border border-text_gray text-black text-[13.03px] bg-transparent rounded-[37.22px] md:py-7 px-10">Case Studies</Button>
+        <div className="md:block hidden">
+          <h1 className="font-bold text-[64px] leading-[83.2px] font-specify_exp_med py-10">Recent Projects</h1>
+          <div className="flex items-center gap-[18.61px]">
+            <Button onClick={(e) => handleCategories(e, "All")} className={`${CATEGORY === "All" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>All</Button>
+            <Button onClick={(e) => handleCategories(e, "app")} className={`${CATEGORY === "app" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>Applications</Button>
+            <Button onClick={(e) => handleCategories(e, "Website")} className={`${CATEGORY === "Website" ? "bg-black text-white" : ""} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 px-10`}>Website</Button>
+            <Button className="blue_footer_hover border border-text_gray text-black text-[13.03px] bg-transparent rounded-[37.22px] md:py-7 px-10">Case Studies</Button>
+          </div>
+          <div className="py-10">
+            <Table className="overflow-hidden">
+              <TableHeader>
+                <TableRow className="border-b border-[#636363]">
+                  <TableHead className="md:min-w-[400px] w-fit">CLIENT</TableHead>
+                  <TableHead>LOCATION</TableHead>
+                  <TableHead>SERVICES</TableHead>
+                </TableRow>
+              </TableHeader>
+              <Work category={CATEGORY} />
+            </Table>
+          </div>
         </div>
-        <div className="py-10">
-          <Table className="overflow-hidden">
-            <TableHeader>
-              <TableRow className="border-b border-[#636363]">
-                <TableHead className="md:min-w-[400px] w-fit">CLIENT</TableHead>
-                <TableHead>LOCATION</TableHead>
-                <TableHead>SERVICES</TableHead>
-              </TableRow>
-            </TableHeader>
-            <Work category={CATEGORY} />
-          </Table>
+
+        <div className="md:hidden block">
+          <WorkMobile />
         </div>
       </div>
       <Footer />
