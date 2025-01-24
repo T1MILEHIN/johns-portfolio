9cddbf7a7549e514c785602a39653d7c6ccf9380
Timi <tunmisebabade@gmail.com>
2025-01-24 17:09:52 +0100

    first commit

--- src/pages/graphics/graphics.jsx
+++ src/pages/graphics/graphics.jsx
@@ -6,12 +6,8 @@ import LaptopAnimation from "./components/laptopAnimation";
 const Graphics = () => {
   return (
     <>
-      <div className="">
-        <div className="px-2 md:px-20 lg:pt-36 pt-60 pb-0 relative">
-          <LandingProfile />
-        </div>
-        <LaptopAnimation />
-      </div>
+      <LandingProfile />
+      <LaptopAnimation />
       <Flyers />
       <Footer />
     </>
