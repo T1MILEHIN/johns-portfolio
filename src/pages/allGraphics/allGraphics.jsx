2bce70d02401c70f6add4f00728cb36ced1c7dfa
Timi <tunmisebabade@gmail.com>
2025-02-26 18:00:38 +0100

    first commit

--- src/pages/allGraphics/allGraphics.jsx
+++ src/pages/allGraphics/allGraphics.jsx
@@ -29,7 +29,7 @@ const AllGraphics = () => {
                         <h1 className="md:w-[686px] w-full font-bold text-[64px] leading-[83.2px] font-specify_exp_med py-10">Graphic Designs Library</h1>
                         <div className="flex items-center gap-[18.61px]">
                             <Link to="/alldesigns">
-                                <Button className={`${pathname === "/alldesigns" ? "bg-black text-white" : pathname === "/alldesigns/logodesigns" ? "" : "bg-white"} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 py-[25px] px-10 shadow-none`}>Flyer Design</Button>
+                                <Button className={`${pathname === "/alldesigns" ? "bg-black text-white hover:bg-white hover:text-black" : pathname === "/alldesigns/logodesigns" ? "hover:bg-white hover:text-black" : "bg-white hover:bg-black hover:text-white"} border border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 py-[25px] px-10 shadow-none`}>Flyer Design</Button>
                             </Link>
                             <Link to="logodesigns">
                                 <Button className={`${pathname === "/alldesigns/logodesigns" ? "bg-bodybg text-black" : "bg-white"} border hover:bg-black hover:text-white border-text_gray text-[13.03px] rounded-[37.22px] md:py-7 py-[25px] px-10 shadow-none`}>Logo Design</Button>
