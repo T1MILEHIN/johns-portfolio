e62669288eca067150737eb6a54eb7ebaf2d7582
Timi <tunmisebabade@gmail.com>
2025-03-06 19:23:17 +0100

    first commit

--- src/pages/ui_ux/landing.jsx
+++ src/pages/ui_ux/landing.jsx
@@ -18,6 +18,15 @@ import ScrollSlide from "./components/scrollSlide";
 import HoverEffect from "../../components/custom/hoverEffect";
 import { Parallax } from 'react-scroll-parallax';
 import { Link } from "react-router-dom";
+import { Navigation, Pagination } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import { fadeIn } from "../variant";
 
 const Landing = () => {
@@ -109,7 +118,60 @@ const Landing = () => {
                 <div className="text-center py-10">
                     <h1 className="font-bold uppercase text-2xl md:text-3xl">Testimonials</h1>
                 </div>
-                <motion.div ref={testimonialRef} className="flex justify-center items-center flex-wrap lg:flex-nowrap gap-16 md:gap-10 md:p-20">
+                <div className="md:hidden block py-10">
+                    <Swiper
+                        modules={[Pagination]}
+                        slidesPerView={1}
+                        pagination={{ clickable: true }}
+                    >
+                        <SwiperSlide>
+                            <div>
+                                <p className="text-[#C6C3C3] font-bold text-xl md:text-sm py-2">01</p>
+                                <div className="py-6 md:py-12 border-y-2 border-[#C6C3C3] flex flex-col gap-4">
+                                    <div className="flex items-center gap-4">
+                                        <img src={tesOne} alt="" className="w-10" />
+                                        <div className="flex flex-col gap-1 text-sm">
+                                            <p className="font-bold">John Doe</p>
+                                            <p className="text-[#636363] text-xs">CEO, XYZ Corporation</p>
+                                        </div>
+                                    </div>
+                                    <p className="text-darkbg leading-9 md:leading-7 text-base md:text-[12px]">Johnbeloved transformed our interface into a visually stunning experience. Their attention to detail and understanding of our brand made the collaboration seamless. Highly recommended!</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="origin-center bg-bodybg">
+                                <p className="text-[#C6C3C3] font-bold text-xl md:text-sm py-2">02</p>
+                                <div className="py-6 md:py-12 border-t-2 border-b border-[#C6C3C3] flex flex-col gap-4">
+                                    <div className="flex items-center gap-4">
+                                        <img src={tesOne} alt="" className="w-10" />
+                                        <div className="flex flex-col gap-1 text-sm">
+                                            <p className="font-bold">John Doe</p>
+                                            <p className="text-[#636363] text-xs">CEO, XYZ Corporation</p>
+                                        </div>
+                                    </div>
+                                    <p className="text-darkbg leading-9 md:leading-7 text-base md:text-[12px]">Johnbeloved optimized our website for conversions with their UI/UX expertise. Their graphics captured our brand essence perfectly. Highly recommended for any design project.</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div>
+                                <p className="text-[#C6C3C3] font-bold text-xl md:text-sm py-2">03</p>
+                                <div className="py-6 md:py-12 border-y-2 border-[#C6C3C3] flex flex-col gap-4">
+                                    <div className="flex items-center gap-4">
+                                        <img src={tesOne} alt="" className="w-10" />
+                                        <div className="flex flex-col gap-1 text-sm">
+                                            <p className="font-bold">John Doe</p>
+                                            <p className="text-[#636363] text-xs">Founder; Tech Startup Co</p>
+                                        </div>
+                                    </div>
+                                    <p className="text-darkbg leading-9 md:leading-7 text-base md:text-[12px]">Johnbeloved Designs boosted our platform&apos;s engagement significantly. His ability to execute complex concepts sets him apart. A designer who delivers results.</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+                <motion.div ref={testimonialRef} className="md:flex hidden justify-center items-center flex-wrap lg:flex-nowrap gap-16 md:gap-10 md:p-20">
                     <motion.div>
                         <motion.p variants={fadeIn("right", 0.1, 0.4)} initial="hidden" whileInView={"show"} viewport={{ once: true, amount: 0.5 }} className="text-[#C6C3C3] font-bold text-xl md:text-sm py-2">01</motion.p>
                         <motion.div variants={fadeIn("down", 0.2, 0.4)} initial="hidden" whileInView={"show"} viewport={{ once: true, amount: 0.5 }} className="py-6 md:py-12 border-y-2 border-[#C6C3C3] flex flex-col gap-4">
