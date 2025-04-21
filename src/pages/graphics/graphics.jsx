543274e3437882a65f57d575de42d99610cad314
Timi <tunmisebabade@gmail.com>
2025-04-21 14:36:01 +0100

    new

--- src/pages/graphics/graphics.jsx
+++ src/pages/graphics/graphics.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
 import Flyers from "./components/flyers";
-import Footer from "../../components/footer"
 import LandingProfile from "../../components/landingProfile";
 import LaptopAnimation from "./components/laptopAnimation";
 import transition from "../../transition"
